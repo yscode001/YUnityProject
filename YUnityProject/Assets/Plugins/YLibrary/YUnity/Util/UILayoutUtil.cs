4d34bdf0225d747564d833afed2a001722f89715
ys <yscode@126.com>
2026-04-01 14:50:09 +0800

    拉伸布局

--- YUnityProject/Assets/Plugins/YLibrary/YUnity/Util/UILayoutUtil.cs
+++ YUnityProject/Assets/Plugins/YLibrary/YUnity/Util/UILayoutUtil.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 namespace YUnity
 {
-    public enum HorizontalType
+    public enum XAxisPos
     {
         Left, Cener, Right
     }
-    public enum VerticalType
+    public enum YAxisPos
     {
-        Left, Cener, Right
+        Top, Cener, Bottom
     }
     public static class UILayoutUtil
     {
         /// <summary>
-        /// 四周拉伸(内置：Inset向里为正，向外为负)
+        /// 四周拉伸(Inset向里为正，向外为负)
         /// </summary>
         /// <param name="rt"></param>
-        /// <param name="leftInset">内置：Inset向里为正，向外为负</param>
-        /// <param name="rightInset">内置：Inset向里为正，向外为负</param>
-        /// <param name="topInset">内置：Inset向里为正，向外为负</param>
-        /// <param name="bottomInset">内置：Inset向里为正，向外为负</param>
+        /// <param name="leftInset">Inset向里为正，向外为负</param>
+        /// <param name="rightInset">Inset向里为正，向外为负</param>
+        /// <param name="topInset">Inset向里为正，向外为负</param>
+        /// <param name="bottomInset">Inset向里为正，向外为负</param>
         public static void Stretch(RectTransform rt, float leftInset, float rightInset, float topInset, float bottomInset)
         {
             if (rt != null)
@@ -33,31 +33,31 @@ namespace YUnity
         }
 
         /// <summary>
-        /// 垂直拉伸，水平固定(内置：Inset向里为正，向外为负)
+        /// 垂直拉伸，水平固定(Inset向里为正，向外为负)
         /// </summary>
         /// <param name="rt"></param>
-        /// <param name="horizontalType"></param>
-        /// <param name="topInset">(内置：Inset向里为正，向外为负)</param>
-        /// <param name="bottomInset">(内置：Inset向里为正，向外为负)</param>
+        /// <param name="xAxisPos"></param>
+        /// <param name="topInset">Inset向里为正，向外为负</param>
+        /// <param name="bottomInset">Inset向里为正，向外为负</param>
         /// <param name="posX"></param>
         /// <param name="width"></param>
-        public static void StretchVertical(RectTransform rt, HorizontalType horizontalType, float topInset, float bottomInset, float posX, float width)
+        public static void StretchVertical(RectTransform rt, XAxisPos xAxisPos, float topInset, float bottomInset, float posX, float width)
         {
             if (rt != null)
             {
-                switch (horizontalType)
+                switch (xAxisPos)
                 {
-                    case HorizontalType.Cener:
+                    case XAxisPos.Cener:
                         rt.pivot = Vector2.one * 0.5f;
                         rt.anchorMin = new Vector2(0.5f, 0);
                         rt.anchorMax = new Vector2(0.5f, 1);
                         break;
-                    case HorizontalType.Left:
+                    case XAxisPos.Left:
                         rt.pivot = new Vector2(0, 0.5f);
                         rt.anchorMin = Vector2.zero;
                         rt.anchorMax = new Vector2(0, 1);
                         break;
-                    case HorizontalType.Right:
+                    case XAxisPos.Right:
                         rt.pivot = new Vector2(1, 0.5f);
                         rt.anchorMin = new Vector2(1, 0);
                         rt.anchorMax = new Vector2(1, 1);
@@ -79,16 +79,49 @@ namespace YUnity
         }
 
         /// <summary>
-        /// 水平拉伸、垂直固定(内置：Inset向里为正，向外为负)
+        /// 水平拉伸、垂直固定(Inset向里为正，向外为负)
         /// </summary>
         /// <param name="rt"></param>
-        /// <param name="verticalType"></param>
-        /// <param name="leftInset">内置：Inset向里为正，向外为负</param>
-        /// <param name="rightInset">内置：Inset向里为正，向外为负</param>
+        /// <param name="yAxisPos"></param>
+        /// <param name="leftInset">Inset向里为正，向外为负</param>
+        /// <param name="rightInset">Inset向里为正，向外为负</param>
         /// <param name="posY"></param>
         /// <param name="height"></param>
-        public static void StretchHorizontal(RectTransform rt, VerticalType verticalType, float leftInset, float rightInset, float posY, float height)
+        public static void StretchHorizontal(RectTransform rt, YAxisPos yAxisPos, float leftInset, float rightInset, float posY, float height)
         {
+            if (rt != null)
+            {
+                switch (yAxisPos)
+                {
+                    case YAxisPos.Cener:
+                        rt.pivot = Vector2.one * 0.5f;
+                        rt.anchorMin = new Vector2(0, 0.5f);
+                        rt.anchorMax = new Vector2(1, 0.5f);
+                        break;
+                    case YAxisPos.Top:
+                        rt.pivot = new Vector2(0.5f, 1);
+                        rt.anchorMin = new Vector2(0, 1);
+                        rt.anchorMax = Vector2.one;
+                        break;
+                    case YAxisPos.Bottom:
+                        rt.pivot = new Vector2(0.5f, 0);
+                        rt.anchorMin = Vector2.zero;
+                        rt.anchorMax = new Vector2(1, 0);
+                        break;
+                    default: break;
+                }
+
+                rt.offsetMin = new Vector2(leftInset, 0);
+                rt.offsetMax = new Vector2(-rightInset, 0);
+
+                Vector2 anchoredPos = rt.anchoredPosition;
+                anchoredPos.y = posY;
+                rt.anchoredPosition = anchoredPos;
+
+                Vector2 sizeDelta = rt.sizeDelta;
+                sizeDelta.y = height;
+                rt.sizeDelta = sizeDelta;
+            }
         }
     }
 }
