051b04a8a33882e3b3d2c48411331f35f1ea5165
ys <yscode@126.com>
2026-05-29 14:30:27 +0800

    增加 pop until

---
+++ YUnityProject/Assets/Plugins/YLibrary/YUnity/ManagerInstance/UIManager/UIStackMag_PopUntil.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace YUnity
+{
+    public partial class UIStackMag
+    {
+        /// <summary>
+        /// 一直pop下去，直到遇到wndName为止，wndName不会pop
+        /// </summary>
+        /// <param name="wndName"></param>
+        /// <param name="popReason"></param>
+        /// <param name="complete"></param>
+        public void PopUntil(string wndName, PopReason popReason, Action<bool> complete = null)
+        {
+            if (_stack.Count == 0 || IsPushingOrPoping)
+            {
+                complete?.Invoke(false);
+                return;
+            }
+            int willPopCount = 0;
+            for (int i = _stack.Count - 1; i >= 0; i--)
+            {
+                if (_stack[i].name == wndName)
+                {
+                    break;
+                }
+                else
+                {
+                    willPopCount += 1;
+                }
+            }
+            if (willPopCount == 0)
+            {
+                complete?.Invoke(false);
+            }
+            else
+            {
+                PopCount(willPopCount, popReason, complete);
+            }
+        }
+
+        /// <summary>
+        /// 一直pop下去，直到遇到wnd为止，wnd不会pop
+        /// </summary>
+        /// <param name="wnd"></param>
+        /// <param name="popReason"></param>
+        /// <param name="complete"></param>
+        public void PopUntil(UIStackBaseWnd wnd, PopReason popReason, Action<bool> complete = null)
+        {
+            if (_stack.Count == 0 || IsPushingOrPoping)
+            {
+                complete?.Invoke(false);
+                return;
+            }
+            int willPopCount = 0;
+            for (int i = _stack.Count - 1; i >= 0; i--)
+            {
+                if (_stack[i] == wnd)
+                {
+                    break;
+                }
+                else
+                {
+                    willPopCount += 1;
+                }
+            }
+            if (willPopCount == 0)
+            {
+                complete?.Invoke(false);
+            }
+            else
+            {
+                PopCount(willPopCount, popReason, complete);
+            }
+        }
+    }
+}
