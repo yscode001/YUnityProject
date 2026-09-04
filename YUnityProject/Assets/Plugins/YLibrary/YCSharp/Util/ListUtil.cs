95a37a412764786e2932b97ff192d70f481a2760
ys <yscoder@icloud.com>
2026-09-04 18:24:16 +0800

    扩展

--- YUnityProject/Assets/Plugins/YLibrary/YCSharp/Util/ListUtil.cs
+++ YUnityProject/Assets/Plugins/YLibrary/YCSharp/Util/ListUtil.cs
@@ -4,22 +4,21 @@ using System.Linq;
 
 namespace YCSharp
 {
-    public static class HashSetUtil
+    public static class ListUtil
     {
         /// <summary>
         /// 求交集、A独有、B独有
         /// </summary>
-        public static void CalcDiff<T>(HashSet<T> a, HashSet<T> b,
-            out (HashSet<T> intersect, HashSet<T> aOnly, HashSet<T> bOnly) result)
+        public static void CalcDiff<T>(List<T> a, List<T> b,
+            out (List<T> intersect, List<T> aOnly, List<T> bOnly) result)
         {
             if (a == null)
                 throw new ArgumentNullException(nameof(a));
             if (b == null)
                 throw new ArgumentNullException(nameof(b));
 
-            var intersect = new HashSet<T>(a.Comparer);
-            var aOnly = new HashSet<T>(a.Comparer);
-            var bOnly = new HashSet<T>(b.Comparer);
+            var intersect = new List<T>();
+            var aOnly = new List<T>();
 
             foreach (var item in a)
             {
@@ -29,10 +28,7 @@ namespace YCSharp
                     aOnly.Add(item);
             }
 
-            foreach (var item in b.Where(item => !a.Contains(item)))
-            {
-                bOnly.Add(item);
-            }
+            var bOnly = b.Where(item => !a.Contains(item)).ToList();
 
             result = (intersect, aOnly, bOnly);
         }
